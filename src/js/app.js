dd10fb485fab6e9c7cb54b39b07aa593095ba10a
Emma Olsson <emmacolsson@hotmail.com>
2021-10-19 12:52:41 +0200

    index html file changes on load as instructed!

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+
 const news = [
     {
         "headline": "Regeringen skjuter på publiklättnader",
@@ -16,8 +17,17 @@ const news = [
         "author": ["Jakob Runevad Kjellmer", "Lydia Farran-Lee", "Sebastian Folcker"],
         "href": "https://www.svt.se/kultur/nobelpriset-i-litteratur-2020",
         "summary": "Den amerikanska poeten Louise Glück mottar Nobelpriset i litteratur 2020. Hon debuterade med Firstborn 1968 och anses vara en av de mest tongivande samtida amerikanska poeterna."
+    
     },
+    
 ]
+
+
+
+
+
+
+
 
 /** 
  * 
@@ -36,3 +46,60 @@ const news = [
  */
 
  console.log('js loaded!')
+
+ document.querySelector('#ex01').textContent = 'DONE'
+
+ document.querySelector('.ex02').style.color = 'red';
+
+ document.querySelector('blockquote').textContent = 'Im awesome'
+
+ const ul = document.querySelector('#shopping-list');
+ let li = document.createElement('li');
+ li.appendChild(document.createTextNode('Milk'));
+ ul.appendChild(li);
+
+ let counter = 1
+ document.addEventListener("click", function(){
+ document.querySelector('#click-counter').innerHTML = counter++;
+ });
+
+ const btn = document.querySelector('button')
+ btn.addEventListener('click', function(){
+ btn.disabled = true 
+ });
+ 
+ document.querySelector('#link-to-svt').addEventListener("click", function(event){
+ event.preventDefault()
+ });
+ 
+ document.querySelector('img').src="https://images.unsplash.com/photo-1634496495743-1492d470f741?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1828&q=80";
+
+
+
+const newsArticle = document.querySelector('#news')
+newsArticle.appendChild(document.createElement('ul'))
+
+news.forEach(
+    n => {
+            const li = document.createElement('li')
+            const header = document.createElement('h1')
+            const author = document.createElement('h2')
+            author.className = 'authorID'
+            const a = document.createElement('a')
+            const p = document.createElement('p')
+            const t1 = document.createTextNode(n.headline)
+            const t2 = document.createTextNode(n.author)
+            const t3 = document.createTextNode(n.href)
+            const t4 = document.createTextNode(n.summary)
+            header.appendChild(t1)
+            author.appendChild(t2)
+            a.appendChild(a.href=t3)
+            p.appendChild(t4)
+            li.appendChild(header)
+            li.appendChild(author)
+            li.appendChild(a)
+            li.appendChild(p)
+            newsArticle.appendChild(li)
+    }
+
+)
